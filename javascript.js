a58eb62eac5c30177b2fcc09c8ac91998a26b144
TheQiBandit <164599241+TheQiBandit@users.noreply.github.com>
2024-05-24 17:37:39 +0300

    Add remove grid button
    Added a remove grid button that deletes the previous
    grid and generates a new one based on user input.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,15 @@
 
+let htmlBody = document.querySelector("body");
 let container = document.querySelector(".container");
 
-function generateGrid(e = 10) {
+
+function generateGrid(e = 16) {
     let tempElem = new DocumentFragment();
     let gridNum = e * e;
     
     for (let i = 0; i < gridNum; i++) {
         
-        let divElem = document.createElement("div");
+        var divElem = document.createElement("div");
         divElem.classList.add("box");
         const boxDimensions = `${760 / e}px`;
         divElem.style.setProperty("width", boxDimensions);
@@ -16,13 +18,27 @@ function generateGrid(e = 10) {
 
         tempElem.appendChild(divElem);
     };
-
-    container.appendChild(tempElem); 
     
+    container.appendChild(tempElem);
+   
 };
 
-generateGrid(100);
-//let userGrid = generateGrid(parseInt(prompt("Enter a number from 1-100")))
+generateGrid(64);
+
+container.addEventListener("mouseover", event => {
+    if (event.target.classList.contains('box')) {
+        event.target.style.backgroundColor = randomizeRGB();
+    }
+}); 
+
+
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+    container.innerHTML = "";
+    generateGrid(prompt("Enter a number from 1-100"));
+});
+
 
 
 function randomizeNum(num) {
@@ -32,10 +48,4 @@ function randomizeNum(num) {
 function randomizeRGB() {
     let rndmColor = `rgb(${randomizeNum(255)} ${randomizeNum(255)} ${randomizeNum(255)})`;
     return rndmColor;
-}
-
-container.addEventListener("mouseover", event => {
-    if (event.target.classList.contains('box')) {
-        event.target.style.backgroundColor = randomizeRGB();
-    }
-});
+};
